114c9c4630986b6d0ec1302e1287bd1c06ac15a4
Jeremy Walker <jez.walker@gmail.com>
2012-04-14 17:22:26 +0100

    Added account model

---
+++ Dinero/Models/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dinero.Models
+{
+    class Account
+    {
+        #region Static Methods
+        public static List<Account> GetAccounts()
+        {
+            return new List<Account>() {
+                new Account("HSBC Current"),
+                new Account("HSBC Savings"),
+                new Account("Santandar"),
+                new Account("Natwest"),
+            };
+        }
+        #endregion
+
+        #region Constructors
+        public Account(string name) 
+        {
+            Name = name;
+        }
+        #endregion
+
+        #region Properties
+        public string Name { get; private set; }
+        #endregion
+    }
+}
